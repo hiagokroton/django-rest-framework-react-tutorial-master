c9d02996602d63d3fd0e79738248a951bc840e19
hiagokroton <41380216+hiagokroton@users.noreply.github.com>
2018-11-08 17:11:01 -0200

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,28 @@
 // App.js
 import React, { Component } from 'react';
 
-const list = [
-  {
-    'id': 1,
-    'title': '1st Item',
-    'description': 'Description here.'
-  },
-  {
-    'id': 2,
-    'title': '2nd Item',
-    'description': 'Another description here.'
-  },
-  {
-    'id': 3,
-    'title': '3rd Item',
-    'description': 'Third description here.'
-  },
-  {
-    'id': 4,
-    'title': 'teste',
-    'description': 'teste.'
-  }
-];
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { list };
+  state = {
+    todos: []
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://127.0.0.1:8000/api/');
+      const todos = await res.json();
+      this.setState({
+        todos
+      });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
-          <div>
+        {this.state.todos.map(item => (
+          <div key={item.id}>
             <h1>{item.title}</h1>
             <span>{item.description}</span>
           </div>
